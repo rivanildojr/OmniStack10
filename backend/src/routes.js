1581f991a65ba1191b6dccd759558d719c57e3f9
rivanildojr <rivanildo_junior7@hotmail.com>
2020-01-15 17:51:57 -0300

    criada a base da aplicacao DevRadar

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const DevController = require("./controllers/DevController");
+const SearchController = require("./controllers/SearchController");
+
+const routes = Router();
+
+//Métodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parâmetros:
+/**
+ * Query Params: req.query (Filtros, ordenação, paginação, ...)
+ * Route Params: req.params (Identificar um recurso na alteração ou remoção)
+ * Body: req.body (Dados para criação ou alteração de um registro)
+ */
+
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
+
+routes.get("/search", SearchController.index);
+
+module.exports = routes;
